cd1685f9f5f85072ce3e0a5070359876c143eef8
NISHANT SHARMA <nishant735123@gmail.com>
2026-07-01 14:07:46 +0530

    Replace import.meta.glob with static imports for Next.js compat

--- client/src/data/flowerCatalog.js
+++ client/src/data/flowerCatalog.js
@@ -1,7 +1,34 @@
-const flowerModules = import.meta.glob("../assets/flowers/**/*.{png,jpg,jpeg,webp,svg}", {
-  eager: true,
-  import: "default",
-});
+import hydrangea_1 from "../assets/flowers/hydrangea/hydrangea_1.png";
+import rose_1 from "../assets/flowers/rose/rose_1.png";
+import rose_2 from "../assets/flowers/rose/rose_2.png";
+import rose_3 from "../assets/flowers/rose/rose_3.png";
+import rose_4 from "../assets/flowers/rose/rose_4.png";
+import rose_5 from "../assets/flowers/rose/rose_5.png";
+import sunflower_1 from "../assets/flowers/sunflower/sunflower_1.png";
+import tulip_1 from "../assets/flowers/tulip/tulip_1.png";
+import tulip_deep_purple_4 from "../assets/flowers/tulip/tulip_deep_purple_4.png";
+import tulip_lavender_1 from "../assets/flowers/tulip/tulip_lavender_1.png";
+import tulip_lilac_2 from "../assets/flowers/tulip/tulip_lilac_2.png";
+import tulip_mauve_5 from "../assets/flowers/tulip/tulip_mauve_5.png";
+import tulip_plum_6 from "../assets/flowers/tulip/tulip_plum_6.png";
+import tulip_violet_3 from "../assets/flowers/tulip/tulip_violet_3.png";
+
+const flowerModules = {
+  "../assets/flowers/hydrangea/hydrangea_1.png": hydrangea_1,
+  "../assets/flowers/rose/rose_1.png": rose_1,
+  "../assets/flowers/rose/rose_2.png": rose_2,
+  "../assets/flowers/rose/rose_3.png": rose_3,
+  "../assets/flowers/rose/rose_4.png": rose_4,
+  "../assets/flowers/rose/rose_5.png": rose_5,
+  "../assets/flowers/sunflower/sunflower_1.png": sunflower_1,
+  "../assets/flowers/tulip/tulip_1.png": tulip_1,
+  "../assets/flowers/tulip/tulip_deep_purple_4.png": tulip_deep_purple_4,
+  "../assets/flowers/tulip/tulip_lavender_1.png": tulip_lavender_1,
+  "../assets/flowers/tulip/tulip_lilac_2.png": tulip_lilac_2,
+  "../assets/flowers/tulip/tulip_mauve_5.png": tulip_mauve_5,
+  "../assets/flowers/tulip/tulip_plum_6.png": tulip_plum_6,
+  "../assets/flowers/tulip/tulip_violet_3.png": tulip_violet_3,
+};
 
 function titleCase(value) {
   return value
@@ -32,8 +59,9 @@ function parseMetaFromPath(path, index) {
 
 export const flowers = Object.entries(flowerModules)
   .sort(([a], [b]) => a.localeCompare(b, undefined, { numeric: true }))
-  .map(([path, src], index) => {
+  .map(([path, imgObj], index) => {
     const meta = parseMetaFromPath(path, index);
+    const src = imgObj && typeof imgObj === "object" ? imgObj.src : imgObj;
     return {
       id: path,
       src,
